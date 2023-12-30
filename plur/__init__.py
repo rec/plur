afb9b4a46f171d04382ec1d1f3decefcf9b87f06
Tom Ritchford <tom@swirly.com>
2023-12-30 14:20:39 +0100

    Add typing and toolchain fixes

--- plur/__init__.py
+++ plur/__init__.py
@@ -1,62 +1,48 @@
-from numbers import Number
-from typing import Sequence, Tuple, Union
-import sys
+import typing as t
 
-__all__ = 'DEFAULT_PLURALS', 'plur',
+__all__ = (
+    "DEFAULT_PLURALS",
+    "plur",
+)
 
-DEFAULT_PLURALS = ['-s']
-
-HasCount = Union[Number, Sequence]
-Plural = Union[HasCount, str]
-Plurals = Tuple[Plural, ...]
+DEFAULT_PLURALS = ("-s",)
 
 
 def plur(
     word: str,
-    *plurals: Plurals,
-    sep: str = ' ',
+    *plurals: str,
+    sep: str = " ",
     num_first: bool = True,
-    zero: str = '',
-) -> str:
-    """ `plur()` pluralizes nouns.
+    zero: str = "",
+) -> t.Callable[[t.Union[int, t.Sequence]], str]:
+    """`plur()` returns a pluralizer
 
-      word:
-          The root word for a single item
+    word:
+        The root word for a single item
 
-      plurals:
-           Zero or more plurals.  If none are given, '-s' is used
+    plurals:
+         Zero or more plurals.  If none are given, '-s' is used
 
-      sep:
-           The separator string between the number and the noun
+    sep:
+         The separator string between the number and the noun
 
-      num_first:
-           A boolean saying whether the number is written first or second
+    num_first:
+         A boolean saying whether the number is written first or second
 
-      zero:
-           If there is a special case for zero items, it goes here
+    zero:
+         If there is a special case for zero items, it goes here
     """
-    deferred = not plurals or isinstance(plurals[-1], str)
-    if not deferred:
-        *plurals, to_count = plurals
-
     plurals = plurals or DEFAULT_PLURALS
     zero = zero or plurals[-1]
     words = zero, word, *plurals
 
-    def plur(n: HasCount) -> str:
-        if not isinstance(n, Number):
-            n = len(n)
-
+    def pluralizer(count: t.Union[int, t.Sequence]) -> str:
+        n = len(count) if isinstance(count, t.Sequence) else count
         i = min(n, len(words) - 1)
         p = words[i]
-        if p.startswith('-'):
+        if p.startswith("-"):
             p = word + p[1:]
 
-        return f'{n}{sep}{p}' if num_first else f'{p}{sep}{n}'
+        return f"{n}{sep}{p}" if num_first else f"{p}{sep}{n}"
 
-    return plur if deferred else plur(to_count)
-
-
-if __name__ != '__main__':
-    [setattr(plur, k, globals()[k]) for k in __all__ + ('__all__',)]
-    sys.modules[__name__] = plur
+    return pluralizer
--- test_plur.py
+++ test_plur.py
@@ -1,37 +1,34 @@
-import plur
+from plur import plur
 
 
 def test_simple():
-    assert plur('dog', 0) == '0 dogs'
-    assert plur('dog', 1) == '1 dog'
-    assert plur('dog', 2) == '2 dogs'
+    assert plur("dog")(0) == "0 dogs"
+    assert plur("dog")(1) == "1 dog"
+    assert plur("dog")(2) == "2 dogs"
 
 
 def test_ending():
-    assert plur('ox', '-en', 0) == '0 oxen'
-    assert plur('ox', '-en', 1) == '1 ox'
-    assert plur('ox', '-en', 2) == '2 oxen'
+    assert plur("ox", "-en")(0) == "0 oxen"
+    assert plur("ox", "-en")(1) == "1 ox"
+    assert plur("ox", "-en")(2) == "2 oxen"
 
 
 def test_defered():
-    dog = plur('dog', zero='doggoes')
+    dog = plur("dog", zero="doggoes")
 
-    assert dog(0) == '0 doggoes'
-    assert dog(1) == '1 dog'
-    assert dog(2) == '2 dogs'
-    assert dog(3) == '3 dogs'
-    assert dog(4) == '4 dogs'
+    assert dog(0) == "0 doggoes"
+    assert dog(1) == "1 dog"
+    assert dog(2) == "2 dogs"
+    assert dog(3) == "3 dogs"
+    assert dog(4) == "4 dogs"
 
 
 def test_exotic():
-    dog = plur(
-        'dog', '-s', '-s', 'pups',
-        zero='doggoes', num_first=False, sep=': '
-    )
+    dog = plur("dog", "-s", "-s", "pups", zero="doggoes", num_first=False, sep=": ")
 
-    assert dog(0) == 'doggoes: 0'
-    assert dog(1) == 'dog: 1'
-    assert dog(2) == 'dogs: 2'
-    assert dog(3) == 'dogs: 3'
-    assert dog(4) == 'pups: 4'
-    assert dog(5) == 'pups: 5'
+    assert dog(0) == "doggoes: 0"
+    assert dog(1) == "dog: 1"
+    assert dog(2) == "dogs: 2"
+    assert dog(3) == "dogs: 3"
+    assert dog(4) == "pups: 4"
+    assert dog(5) == "pups: 5"
